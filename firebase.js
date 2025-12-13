ca7f5cdc9364553d34388e4e2136b74f8fc90ef4
Kencillama <kentomi2020@gmail.com>
2025-12-13 13:27:20 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// login.js と同じ設定（senpainet-84a24）に統一します
+// ★★★ 接続設定を統一 ★★★
 const firebaseConfig = {
   apiKey: "AIzaSyCwPtYMU_xiM5YgcqfNsCFESkj-Y4ICD5E",
   authDomain: "senpainet-84a24.firebaseapp.com",
@@ -11,6 +11,7 @@ const firebaseConfig = {
   appId: "1:1053589632945:web:413919be47760675e4ef90",
   measurementId: "G-1GPKNSMMFZ"
 };
+// ★★★ 接続設定を統一 ★★★
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
