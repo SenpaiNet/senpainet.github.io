1e37842dcaf386d11ad7e54c1d6d3044c82e3782
Kencillama <kentomi2020@gmail.com>
2025-12-13 13:46:40 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// ★★★ 接続設定を統一 ★★★
+// ★★★ 設定を senpainet-auth に統一 ★★★
 const firebaseConfig = {
-  apiKey: "AIzaSyCwPtYMU_xiM5YgcqfNsCFESkj-Y4ICD5E",
-  authDomain: "senpainet-84a24.firebaseapp.com",
-  projectId: "senpainet-84a24",
-  storageBucket: "senpainet-84a24.firebasestorage.app",
-  messagingSenderId: "1053589632945",
-  appId: "1:1053589632945:web:413919be47760675e4ef90",
-  measurementId: "G-1GPKNSMMFZ"
+  apiKey: "AIzaSyDuDU6ujKlBcxP05XOUwPsGqpxQVqeHgvs",
+  authDomain: "senpainet-auth.firebaseapp.com",
+  projectId: "senpainet-auth",
+  storageBucket: "senpainet-auth.firebasestorage.app",
+  messagingSenderId: "694282767766",
+  appId: "1:694282767766:web:3e0dd18f697aafb60e61b7",
+  measurementId: "G-977F3HXN1F"
 };
-// ★★★ 接続設定を統一 ★★★
+// ★★★ 設定ここまで ★★★
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
